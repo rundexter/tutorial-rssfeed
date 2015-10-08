f1fe5c7ba7a743321caedd4ba8af9e94fd395bc7
Justin Fargione <justin@lexicalbits.com>
2015-10-08 15:59:10 -0400

    Added remote call function

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
+var _ = require('lodash'),
+    request = require('request');
 module.exports = {
     run: function(step, dexter) {
+    },
+    fetchUrl: function(url, callback) {
+        var req = request(url);
+        req.on('error', callback);
+        req.on('response', function(resp) {
+            if(resp.statusCode != 200) {
+                return callback(new Error(
+                    'Bad status code ' + resp.statusCode + ' for ' + url
+                ));
+            }
+            return callback(null, this);
+        });
     }
 };
